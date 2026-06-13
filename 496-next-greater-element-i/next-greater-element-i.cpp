49b8eca8dc51099de741720e5488e153f23f733b
prollyparth <parthgerman2006@gmail.com>
2026-06-13 09:59:58 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (37.38%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,19 +1,33 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        stack<int> s;
         unordered_map<int , int> m;
-        for(int i = 0 ; i < nums2.size() ; i++){
-            m[nums2[i]] = -1;
-            for(int j = i + 1 ; j < nums2.size() ; j++){
-                if(nums2[i] < nums2[j]){
-                    m[nums2[i]] = nums2[j];
-                    break;
+        for(int i = nums2.size() - 1 ; i >= 0 ; i--){
+            if(s.empty()){
+                s.push(nums2[i]);
+                m[nums2[i]] = -1;
+            }
+            else if(nums2[i] < s.top()){
+                m[nums2[i]] = s.top();
+                s.push(nums2[i]);
+            }
+            else{
+                while(!s.empty() && nums2[i] >= s.top()){
+                    s.pop();
                 }
+                if(s.empty()){
+                    m[nums2[i]] = -1;
+                }
+                else{
+                    m[nums2[i]] = s.top();
+                }
+                s.push(nums2[i]);
             }
         }
         vector<int> ans;
-        for(int i = 0 ; i < nums1.size() ; i++){
-            ans.push_back(m[nums1[i]]);
+        for(int i : nums1){
+            ans.push_back(m[i]);
         }
         return ans;
     }
